52d18b9e811841334f983936adb8b778168bf821
progfay <shun1856shun@gmail.com>
2019-04-11 11:34:53 +0900

    first commit

---
+++ .hyper.js
@@ -0,0 +1,108 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+    config: {
+        // Choose either "stable" for receiving highly polished,
+        // or "canary" for less polished but more frequent updates
+        updateChannel: 'stable',
+
+        // default font size in pixels for all tabs
+        fontSize: 13,
+
+        // font family with optional fallbacks
+        fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+
+        // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+        cursorColor: 'rgba(248,28,229,0.8)',
+
+        // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+        cursorShape: 'BEAM',
+
+        // set to true for blinking cursor
+        cursorBlink: false,
+
+        // The background color/opacity of the text selection in terminal
+        selectionColor: 'rgba(98, 114, 164, 0.3)',
+
+        // custom css to embed in the main window
+        css: '',
+
+        // custom css to embed in the terminal window
+        termCSS: ``,
+
+        // custom padding (css format, i.e.: `top right bottom left`)
+        padding: '12px 14px',
+
+        // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+        // by default ['--login'] will be used
+        shellArgs: ['--login'],
+
+        // for environment variables
+        env: {},
+
+        // set to false for no bell
+        bell: 'false',
+
+        // if true, selected text will automatically be copied to the clipboard
+        copyOnSelect: false,
+
+        // for advanced config flags please refer to https://hyper.is/#cfg
+
+        // ========================================================================= //
+        // ======================== begin of plugins config ======================== //
+        // ========================================================================= //
+
+        // for https://www.npmjs.com/package/hyper-confirm
+        confirmQuit: true,
+
+        // for https://www.npmjs.com/package/hyper-pane
+        paneNavigation: {
+            debug: false,
+            hotkeys: {
+                navigation: {
+                    up: 'ctrl+alt+up',
+                    down: 'ctrl+alt+down',
+                    left: 'ctrl+alt+left',
+                    right: 'ctrl+alt+right'
+                },
+                jump_prefix: 'ctrl', // completed with 1-9 digits
+                permutation_modifier: 'shift', // Added to jump and navigation hotkeys for pane permutation
+                maximize: 'meta+enter'
+            },
+            showIndicators: true, // Show pane number
+            indicatorPrefix: '^', // Will be completed with pane number
+            indicatorStyle: { // Added to indicator <div>
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                fontSize: '10px',
+                color: 'lightBlue'
+            }
+        }
+
+        // ========================================================================= //
+        // ========================= end of plugins config ========================= //
+        // ========================================================================= //
+    },
+
+    // a list of plugins to fetch and install from npm
+    plugins: [//
+        //
+        'hyper-dark-scrollbar', //
+        'hyper-pane', //
+        "hyper-search", //
+        "hyper-dracula" //
+    ],
+
+    // in development, you can create a directory under
+    // `~/.hyper_plugins/local/` and include it here
+    // to load it and avoid it being `npm install`ed
+    localPlugins: [],
+
+    keymaps: {
+        // Example
+        // 'window:devtools': 'cmd+alt+o',
+    }
+};
---
+++ .zshrc
@@ -0,0 +1,140 @@
+alias vi='vim'
+alias code='open -a Visual\ Studio\ Code.app'
+alias find='find `pwd` -name'
+alias mdfind='mdfind -onlyin . '
+alias mkdir='mkdir -p '
+alias -g C='| pbcopy'
+alias -g L='| less'
+alias cls='clear'
+alias ls='ls -Gp'
+alias la='ls -Ap'
+alias ll='ls -hlap'
+alias o='open'
+alias fuck='toilet --font mono9 --filter border --gay "うんうん
+わかるよ
+そのきもち"'
+function up () {
+    if [ $# -eq 0 ] ; then
+        cd ..
+    else
+        for ((i=0; i < $1; i++))
+        do
+            cd ..
+        done
+    fi
+}
+
+# less
+export LESS='-iMR'
+export LESSOPEN='|lessfilter %s'
+
+# 標準エディタを設定する
+export EDITOR=vim
+export VISUAL=vim
+export PAGER=less
+
+# コマンド履歴を保存するファイルを指定する
+export HISTFILE=~/.zhistory
+
+# メモリに保存する履歴の件数を指定する
+export HISTSIZE=1000
+
+# ファイルに保存する履歴の件数を指定する
+export SAVEHIST=100000
+
+# 文字コードを設定する
+export LANG=ja_JP.UTF-8
+
+export CLICOLOR=true
+export LSCOLORS='gxfxxxxxcxxxxxxxxxxxxx'
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+export HOMEBREW_GITHUB_API_TOKEN="fe8e1575d49834dfd6afda0aef871964c8005926"
+export EMSDK="/Users/fms_eraser/emsdk"
+export EM_CONFIG="/Users/fms_eraser/.emscripten"
+export BINARYEN_ROOT="/Users/fms_eraser/emsdk/clang/e1.38.6_64bit/binaryen"
+export EMSCRIPTEN="/Users/fms_eraser/emsdk/emscripten/1.38.6"
+export GOPATH="/usr/local/bin"
+export MANPATH="$MANPATH:/opt/local/share/man"
+export FPATH="$FPATH:/usr/local/share/zsh/site-functions:/usr/share/zsh/5.3/functions"
+export PATH="$PATH:/Users/fms_eraser/.cargo/bin:/Users/fms_eraser/emsdk:/Users/fms_eraser/emsdk/clang/e1.38.6_64bit:/Users/fms_eraser/emsdk/node/8.9.1_64bit/bin:/Users/fms_eraser/emsdk/emscripten/1.38.6:/Users/fms_eraser/go/bin:/Users/fms_eraser/.vimpkg/bin:/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin"
+
+# enable Ctrl + E, Ctrl + A and more
+bindkey -e
+
+# 補完候補を一覧で表示する
+setopt auto_list
+
+# 補完キー連打で候補順に自動で補完する
+setopt auto_menu
+
+# コマンド中の余分なスペースは削除して履歴に記録する
+setopt hist_reduce_blanks
+
+# 履歴と重複するコマンドを記録しない
+setopt hist_save_no_dups
+
+# ^D でシェルを終了しない
+setopt ignore_eof
+
+# 履歴をすぐに追加する（通常はシェル終了時）
+setopt inc_append_history
+
+# # 以降をコメントとして扱う
+setopt interactive_comments
+
+# ビープを無効にする
+setopt no_beep
+setopt no_hist_beep
+setopt no_list_beep
+
+# = 以降も補完する（例：--option=value）
+setopt magic_equal_subst
+
+# バックグラウンド処理の状態変化をすぐに通知する
+setopt notify
+
+# 8bit文字を有効にする
+setopt print_eight_bit
+
+# 終了ステータスが0以外の場合にステータスを表示する
+setopt print_exit_value
+
+# rm * の前に確認をとる
+setopt rm_star_wait
+
+# コマンド実行後は右プロンプトを消す
+setopt transient_rprompt
+
+# コマンドのオプションや引数を補完する
+autoload -Uz compinit -u # autoload -Uz compinit && compinit -u
+
+# 補完の表示方法を変更する
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' use-cache true
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:default' menu select=2
+zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:options' description 'yes'
+
+# use zplug
+source ~/.zplug/init.zsh
+
+# if ! zplug check --verbose; then
+#   printf 'Install? [y/N]: '
+#   if read -q; then
+#     echo; zplug install
+#   fi
+# fi
+
+zplug 'zsh-users/zsh-autosuggestions'
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-completions"
+zplug load
+
+# for pure
+autoload -U promptinit; promptinit
+prompt pure
+
